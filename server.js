29fbc21640a9d4124ea4b10bff45be05bc77eead
Eric Odell <esodell1@gmail.com>
2018-02-17 15:35:19 -0800

    Test commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
@@ -85,7 +85,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send('{ pageCount: ' + count + '}!');
     });
   } else {
     res.send('{ pageCount: -1 }');
